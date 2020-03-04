ecd7a8d3207f08891788cb1aed20a97ebefc49d5
ohsr <mx.france80@protonmail.com>
2020-03-04 21:08:25 +0100

    Progress API

--- server.js
+++ server.js
@@ -1,22 +1,34 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+require('dotenv').config()
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(express.urlencoded());
 app.use(express.json());
 
+const nodeHtmlToImage = require('node-html-to-image')
+
+
 app.post('/', function (req, res) {
-  console.log(req.body)
   if(req.body.hasOwnProperty("film1") && req.body.hasOwnProperty("film2")){
-    return res.status(200).json({
-      status: "success",
-      message: "Infography"
-    }) 
+    nodeHtmlToImage({
+      output: './image.png',
+      html: '<html><body>Hello world2!</body></html>'
+    })
+    .then(() => {
+      console.log('The image was created successfully!')
+      return res.status(200).json({
+        status: "success",
+        message: "Infography"
+      }) 
+    })
+    
+  }else{
+    return res.status(402).json({
+      status: "fail",
+      message: "Les données envoyées sont incorrects"
+    })
   }
-  return res.status(402).json({
-    status: "fail",
-    message: "Les données envoyées sont incorrects"
-  })
 });
 
 app.get('/', function (req, res) {
